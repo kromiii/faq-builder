175cd6eeb27fa6b4a776bffcfbe69f5c174bd95d
Hiroyuki Kuromiya <15026387+kromiii@users.noreply.github.com>
2023-11-25 11:11:50 +0900

    initial commit

---
+++ faq-builder.php
@@ -0,0 +1,12 @@
+<?php
+/*
+  Plugin Name: FAQ Builder
+  Plugin URI:
+  Description: PDF から FAQ を作成するプラグイン
+  Version: 0.0.1
+  Author: Hiroyuki KUROMIYA
+  Author URI: https://github.com/kromiii
+  License: GPLv2
+ */
+
+?>
